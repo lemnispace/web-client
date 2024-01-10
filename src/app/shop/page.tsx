ca64a1d028611e926d6fc12a19eb28952b06752c
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-01-10 17:10:08 +0000

    Refactor shop page component

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,64 +1,63 @@
 import { ProductGridSection } from "@/app/components/shop/ProductGrid";
+import { Container } from "@/components/container";
+import mosaicCloseupImage from "@/images/mosaic/mosaic-closeup-1.jpg";
+import { classNames } from "@/utils";
 import { PRODUCTS_MAIN_MESSAGE_SECTION_TEXT } from "@/utils/text";
 
-const products = [
-  {
-    id: 1,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$13",
-    description: "3 sizes available",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-01.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 2,
-    name: "Focus Card Holder",
-    href: "#",
-    price: "$64",
-    description: "Walnut",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-02.jpg",
-    imageAlt: "Paper card sitting upright in walnut card holder on desk.",
-  },
-  {
-    id: 3,
-    name: "Focus Carry Pouch",
-    href: "#",
-    price: "$32",
-    description: "Heather Gray",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-03.jpg",
-    imageAlt:
-      "Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.",
-  },
-  // More products...
-];
+interface Product {
+  id: number;
+  name: string;
+  href: string;
+  price: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+}
 
-function ProductsMainMessageSection() {
+interface MainMessageSectionProps extends React.HTMLAttributes<HTMLElement> {
+  title: string;
+  description: string;
+}
+
+function ProductsMainMessageSection({
+  title,
+  description,
+  className,
+  ...props
+}: MainMessageSectionProps) {
   return (
-    <div className="py-24 text-center">
+    <div className={classNames("py-24 text-center", className)} {...props}>
       <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-        {PRODUCTS_MAIN_MESSAGE_SECTION_TEXT.title}
+        {title}
       </h1>
       <p className="mx-auto mt-4 max-w-3xl text-base text-gray-500">
-        {PRODUCTS_MAIN_MESSAGE_SECTION_TEXT.description}
+        {description}
       </p>
     </div>
   );
 }
 
 export default function Shop() {
+  const products = [
+    {
+      id: 1,
+      name: "Focus Paper Refill",
+      href: "/shop/mosaics/1",
+      price: "$13",
+      description: "3 sizes available",
+      imageSrc: mosaicCloseupImage.src,
+      imageAlt: "Text mosaic of a couple kissing",
+    },
+  ];
   return (
-    <div className="bg-gray-50">
-      <main>
-        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <ProductsMainMessageSection />
-          <ProductGridSection products={products} />
-        </div>
-      </main>
-    </div>
+    <main>
+      <Container className="max-w-3xl lg:max-w-7xl">
+        <ProductsMainMessageSection
+          title={PRODUCTS_MAIN_MESSAGE_SECTION_TEXT.title}
+          description={PRODUCTS_MAIN_MESSAGE_SECTION_TEXT.description}
+        />
+        <ProductGridSection products={products} />
+      </Container>
+    </main>
   );
 }
