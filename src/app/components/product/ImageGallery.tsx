62016315e6c9bab9f96975f13d204ad58c9c000d
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-03-31 07:52:17 +0000

    add product fetch

--- src/app/components/product/ImageGallery.tsx
+++ src/app/components/product/ImageGallery.tsx
@@ -1,24 +1,9 @@
 "use client";
 
+import { Product } from "@/utils/types";
 import { Tab } from "@headlessui/react";
 import clsx from "clsx";
 import Image from "next/image";
-
-interface ProductImage {
-  id: number;
-  name: string;
-  src: string;
-  alt: string;
-}
-
-interface Product {
-  id: number;
-  name: string;
-  href: string;
-  price: string;
-  description: string;
-  images: ProductImage[];
-}
 
 interface ImageGalleryProps {
   product: Product;
@@ -35,18 +20,20 @@ export default function ImageGallery({
       {/* Image selector */}
       <div className="mx-auto mt-6 w-full max-w-2xl block lg:max-w-none">
         <Tab.List className="grid grid-cols-4 gap-6">
-          {product.images.map((image) => (
+          {product.images?.map((image, index) => (
             <Tab
-              key={image.id}
+              key={image.src}
               className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 lemni-focus-4"
             >
               {({ selected }) => (
                 <>
-                  <span className="sr-only">{image.name}</span>
+                  <span className="sr-only">
+                    {image.alt ?? `${product.name}-${index}`}
+                  </span>
                   <span className="absolute inset-0 overflow-hidden rounded-md">
                     <Image
                       src={image.src}
-                      alt={image.alt}
+                      alt={image.alt ?? ""}
                       style={{
                         maxWidth: "100%",
                       }}
@@ -71,11 +58,11 @@ export default function ImageGallery({
       </div>
 
       <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
-        {product.images.map((image) => (
-          <Tab.Panel key={image.id} className="lemni-focus-4">
+        {product.images?.map((image) => (
+          <Tab.Panel key={image.src} className="lemni-focus-4">
             <Image
               src={image.src}
-              alt={image.alt}
+              alt={image.alt ?? ""}
               style={{
                 maxWidth: "100%",
               }}
