3ab07716536409f1ba3c2a878b5830c3adb46b1b
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-03-31 00:27:39 +0000

    fetch products from shopify

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,32 +1,62 @@
 import ProductsMainMessageSection from "@/app/components/product/ProductsMainMessageSection";
 import { ProductGridSection } from "@/app/components/shop/ProductGrid";
 import { Container } from "@/components/container";
-import mosaicCloseupImage from "@/images/mosaic/mosaic-closeup-1.jpg";
+import { productClient } from "@/lib/shopify/client";
+import {
+  productsAndVariantsQuery,
+  ProductsAndVariantsResponse,
+} from "@/lib/shopify/queries/productsQuery";
+import { Image } from "@/lib/types/shopify";
+import sanitizeHtml from "@/utils/formatters";
 import { PRODUCTS_MAIN_MESSAGE_SECTION_TEXT } from "@/utils/text";
-import clsx from "clsx";
+import { ProductItem } from "@/utils/types";
 
-interface Product {
-  id: number;
-  name: string;
-  href: string;
-  price: string;
-  description: string;
-  imageSrc: string;
-  imageAlt: string;
+function getProductList(firstNProducts: number, firstNVariants: number) {
+  return productClient.request<ProductsAndVariantsResponse>(
+    productsAndVariantsQuery,
+    {
+      variables: {
+        firstNProducts,
+        firstNVariants,
+      },
+    }
+  );
 }
 
-export default function Shop() {
-  const products = [
-    {
-      id: 1,
-      name: "Focus Paper Refill",
-      href: "/shop/mosaics/text-mosaic",
-      price: "$13",
-      description: "3 sizes available",
-      imageSrc: mosaicCloseupImage.src,
-      imageAlt: "Text mosaic of a couple kissing",
-    },
-  ];
+function mapProducts(productList?: ProductsAndVariantsResponse): ProductItem[] {
+  if (!productList) {
+    return [];
+  }
+  return productList.products.edges.map((e) => {
+    const img = e.node.media.edges[0]?.node.previewImage as Image | undefined;
+    return {
+      id: e.node.id,
+      name: e.node.title,
+      description: e.node.description,
+      descriptionHtml: sanitizeHtml(e.node.descriptionHtml),
+      tags: e.node.tags,
+      priceRange: e.node.priceRange,
+      type: e.node.productType,
+      href: `/shop/mosaics/${e.node.handle}`,
+      img: img
+        ? {
+            alt: img.altText ?? e.node.title,
+            src: img.url,
+            width: img.width,
+            height: img.height,
+          }
+        : undefined,
+      variants: e.node.variants.edges.map((variant) => variant.node),
+    };
+  });
+}
+
+export default async function Shop() {
+  const productList = await getProductList(20, 3);
+  if (productList.errors) {
+    console.error("Error getting product list: ", productList.errors);
+  }
+  const products = mapProducts(productList.data);
   return (
     <main>
       <Container>
