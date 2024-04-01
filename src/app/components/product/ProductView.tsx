86cdc576cd5d9a2e6b77b293da2897d2732df8e0
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-04-01 06:51:54 +0000

    connect image gallery to selected color

--- src/app/components/product/ProductView.tsx
+++ src/app/components/product/ProductView.tsx
@@ -1,5 +1,9 @@
-import { formatPrice } from "@/utils/formatters";
+"use client";
+
+import { ProductVariantOptionType } from "@/lib/types/shopify";
+import { formatPrice, getVariantTitleFromOptions } from "@/utils/formatters";
 import { Product } from "@/utils/types";
+import { useState } from "react";
 import ImageGallery from "./ImageGallery";
 import ProductDescription from "./ProductDescription";
 import ProductSelectionForm from "./ProductSelectionForm";
@@ -8,10 +12,23 @@ import ProductTitle from "./ProductTitle";
 interface ProductViewProps {
   product: Product;
 }
+
+const getVariantValues = (
+  product: Product,
+  variantName: ProductVariantOptionType
+) => {
+  return product.variants?.find((v) => v.name === variantName)?.values ?? [];
+};
+
 export const ProductView = ({ product, ...props }: ProductViewProps) => {
+  const colors = getVariantValues(product, "Color");
+  const sizes = getVariantValues(product, "Size");
+  const [variantKey, setVariantKey] = useState<string>(
+    getVariantTitleFromOptions(colors[0], sizes[0])
+  );
   return (
     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-      <ImageGallery product={product} />
+      <ImageGallery product={product} imageVariantKey={variantKey} />
       {/* Product info */}
       <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
         <ProductTitle
@@ -28,7 +45,13 @@ export const ProductView = ({ product, ...props }: ProductViewProps) => {
             descriptionHtml={product.descriptionHtml}
           />
         </div>
-        <ProductSelectionForm product={product} className="mt-6" />
+        <ProductSelectionForm
+          colors={colors}
+          onColorChange={(newColor) => {
+            setVariantKey(getVariantTitleFromOptions(newColor, sizes[0]));
+          }}
+          className="mt-6"
+        />
         {/* <section aria-labelledby="details-heading" className="mt-12">
           <ProductSectionTitle id="details-heading">
             Additional details
