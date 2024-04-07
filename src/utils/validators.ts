088ab5aaacba4a88f4f54d494f0333f36c87dcba
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-04-07 20:50:10 +0000

    Adds utility functions

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -11,10 +11,14 @@ export const isDefined = <T>(value: T | undefined | null): value is T => {
   return value !== undefined && value !== null;
 };
 
+/**
+ * Represents a product with a variant of a specific type.
+ */
 interface ProductWithVariant<T extends ProductVariantOptionType>
   extends Pick<Product, "variants"> {
   variants: Product["variants"] & { [key in T]: string };
 }
+
 /**
  * Checks if a product has a variant of the specified type.
  *
@@ -27,3 +31,40 @@ export const hasVariant = <T extends ProductVariantOptionType>(
   type: T
 ): product is Product & ProductWithVariant<T> =>
   product.variants?.some((variant) => variant[type]) ?? false;
+
+/**
+ * Checks if a value is a number.
+ *
+ * @param value - The value to check.
+ * @returns `true` if the value is a number, `false` otherwise.
+ */
+export const isNumber = (value: unknown): value is number =>
+  typeof value === "number" && !isNaN(value) && isFinite(value);
+
+/**
+ * Converts a string value to an integer.
+ *
+ * @param value - The string value to convert.
+ * @returns The converted integer value, or `undefined` if the input is not a valid number.
+ */
+export const toInt = (value: string | undefined): number | undefined => {
+  if (!value) {
+    return undefined;
+  }
+  const int = parseInt(value, 10);
+  return isNumber(int) ? int : undefined;
+};
+
+/**
+ * Converts a string value to a float.
+ *
+ * @param value - The string value to convert.
+ * @returns The converted float value, or `undefined` if the input is not a valid number.
+ */
+export const toFloat = (value: string | undefined): number | undefined => {
+  if (!value) {
+    return undefined;
+  }
+  const float = parseFloat(value);
+  return isNumber(float) ? float : undefined;
+};
