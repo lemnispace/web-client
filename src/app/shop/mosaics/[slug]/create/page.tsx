22f99a631421715bff9404601f4d878258640d99
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-04-07 20:53:37 +0000

    Refactor CreateMosaic component to fetch product & variant & handle errors

--- src/app/shop/mosaics/[slug]/create/page.tsx
+++ src/app/shop/mosaics/[slug]/create/page.tsx
@@ -1,9 +1,37 @@
 import ImgEditor from "@/app/components/editor/ImgEditor";
 import ProductsMainMessageSection from "@/app/components/product/ProductsMainMessageSection";
 import { Container } from "@/components/container";
+import { fetchProduct } from "@/lib/shopify/queries/productsQuery";
+import { mapProduct } from "@/utils/mappers";
 import { PRODUCTS_CREATE_MESSAGE_SECTION_TEXT } from "@/utils/text";
+import { redirect } from "next/navigation";
 
-export default function CreateMosaic() {
+interface MosaicProps {
+  params: {
+    slug: string;
+  };
+  searchParams: Record<string, string>;
+}
+
+export default async function CreateMosaic(props: MosaicProps) {
+  const variantId = props.searchParams.variant;
+  const productHandle = props.params.slug;
+  const productResponse = await fetchProduct(productHandle);
+  if (productResponse.errors) {
+    console.error("Error getting product: ", productResponse.errors);
+  }
+  const product = productResponse.data?.product
+    ? mapProduct(productResponse.data.product)
+    : undefined;
+  if (!product) {
+    console.error("Error, product not found");
+    redirect("/not-found");
+  }
+  const variant = product.variants?.find((v) => v.id === variantId);
+  if (!variant) {
+    console.error("Error, variant not found");
+    redirect("/not-found");
+  }
   return (
     <main className="bg-white">
       <Container>
@@ -11,7 +39,7 @@ export default function CreateMosaic() {
           title={PRODUCTS_CREATE_MESSAGE_SECTION_TEXT.title}
           description={PRODUCTS_CREATE_MESSAGE_SECTION_TEXT.description}
         />
-        <ImgEditor />
+        <ImgEditor productVariant={variant} />
       </Container>
     </main>
   );
