2eb777372d413e867d99b5b8ef53eea4e2531428
Santi G. <24705975+santiagoa58@users.noreply.github.com>
2024-04-20 15:07:09 +0000

    split text constants into separate files to improve maintainability & add status messages

---
+++ src/utils/text/layoutPage.ts
@@ -0,0 +1,45 @@
+import { GLOBAL_APP_TEXT } from "./global";
+
+export const LAYOUT_TEXT = {
+  root: {
+    name: GLOBAL_APP_TEXT.name,
+    title: "Customizable E-commerce Platform",
+    shortDescription: "Customizable E-commerce Platform",
+    description: `Explore ${GLOBAL_APP_TEXT.name}, the innovative e-commerce platform specializing in personalized products. Our unique service allows users to create custom text mosaics and other tailor-made items, offering an interactive and creative shopping experience. Ideal for those seeking one-of-a-kind gifts or personal keepsakes.`,
+  },
+  shop: {
+    name: "Shop",
+    description: `Shop ${GLOBAL_APP_TEXT.name}’s collection of customizable products. Our unique service allows users to create custom text mosaics and other tailor-made items, offering an interactive and creative shopping experience. Ideal for those seeking one-of-a-kind gifts or personal keepsakes.`,
+  },
+} as const;
+
+// *** Call To Action Text ***
+export const CTA_TEXT = {
+  title: "Begin Your Creative Journey",
+  shortDescription: "Begin Your Creative Journey",
+  description: `Embark on a path of personal expression with ${GLOBAL_APP_TEXT.name}. Unleash your creativity and transform your vision into bespoke art pieces that tell your unique story.`,
+} as const;
+
+export const COMING_SOON_TEXT = {
+  title: "More Great Products Coming Soon",
+  shortDescription: "More products coming soon",
+  description:
+    "We are working hard to bring you more great products. Please check back soon.",
+} as const;
+
+export const HERO_TEXT = {
+  // break up title into array of strings to allow for styling
+  title: ["Crafting", "your visions", "into artistic treasures"],
+  shortDescription: "Crafting your visions into artistic treasures",
+  description:
+    "Every product is a canvas for your imagination, transforming your ideas into art that celebrates your individuality.",
+} as const;
+
+export const COLLECTION_TEXT = {
+  textMosaic: {
+    title: "Text Mosaic",
+    shortDescription: "Text Mosaic",
+    description:
+      "Create a personalized text mosaic portrait with your own words and messages. Perfect as a gift or a stunning piece of art for your home or office.",
+  },
+} as const;
