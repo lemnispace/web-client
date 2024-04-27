8ec2542b561b5d7f6a27d3d4c10ea362d50b7dd6
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-04-27 09:17:07 -0700

    Add EditorLoader and TextInputDialog components

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,6 +2,7 @@ import DOMPurify from "isomorphic-dompurify";
 
 /**
  * Formats the price with the currency sign.
+ *
  * @param price - The price to format.
  * @param currencyCode - The currency code.
  * @returns The formatted price.
@@ -15,9 +16,26 @@ export const formatPrice = (price: string | number, currencyCode: string) => {
 
 /**
  * Sanitizes the given HTML string using DOMPurify.
+ *
  * @param html - The HTML string to sanitize.
  * @returns The sanitized HTML string.
  */
 export const sanitizeHtml = (html: string) => {
   return DOMPurify.sanitize(html);
+};
+
+/**
+ * Converts a string to kebab-case.
+ *
+ * @param str - The string to convert.
+ * @returns The kebab-case string.
+ */
+export const toKebabCase = (str: string): string => {
+  // Convert camelCase to kebab-case
+  const kebabCase = str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+
+  // Replace spaces with hyphens
+  const finalString = kebabCase.trim().replace(/\s+/g, "-");
+
+  return finalString;
 };
