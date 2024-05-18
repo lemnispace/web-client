9133eb32f9011117de3160f92f2ddee2a9d54c12
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-05-18 12:47:59 -0700

    Refactor validators module and remove duplicated toInt and toFloat functions

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -44,34 +44,6 @@ export const isNumber = (value: unknown): value is number =>
   isFinite(Number(value));
 
 /**
- * Converts a string value to an integer.
- *
- * @param value - The string value to convert.
- * @returns The converted integer value, or `undefined` if the input is not a valid number.
- */
-export const toInt = (value: string | undefined): number | undefined => {
-  if (!value && typeof value !== "number") {
-    return undefined;
-  }
-  const int = parseInt(value, 10);
-  return isNumber(int) ? int : undefined;
-};
-
-/**
- * Converts a string value to a float.
- *
- * @param value - The string value to convert.
- * @returns The converted float value, or `undefined` if the input is not a valid number.
- */
-export const toFloat = (value: string | undefined): number | undefined => {
-  if (!value && typeof value !== "number") {
-    return undefined;
-  }
-  const float = parseFloat(value);
-  return isNumber(float) ? float : undefined;
-};
-
-/**
  * Checks if a value is a string.
  *
  * @param value - The value to check.
