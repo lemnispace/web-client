2f91a7752d34175c432d65a1cbb971b380db781b
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-05-18 10:00:03 -0700

    Add StagedUpload types and interfaces to shopify.ts

--- src/lib/types/shopify.ts
+++ src/lib/types/shopify.ts
@@ -126,6 +126,59 @@ export interface ImageJobNode {
 
 /*
  *  ╔══════════════════════════════════════════════════════════════════════════════╗
+ *  ║                               Staged Upload Types                            ║
+ *  ╚══════════════════════════════════════════════════════════════════════════════╝
+ */
+
+export type StagedUploadTargetGenerateUploadResource =
+  | "BULK_MUTATION_VARIABLES"
+  | "COLLECTION_IMAGE"
+  | "FILE"
+  | "IMAGE"
+  | "MODEL_3D"
+  | "PRODUCT_IMAGE"
+  | "RETURN_LABEL"
+  | "SHOP_IMAGE"
+  | "URL_REDIRECT_IMPORT"
+  | "VIDEO";
+
+export interface StagedUploadTarget {
+  /**
+   * Parameters contain all the sensitive info we'll need to interact with the aws bucket.
+   */
+  parameters: { name: string; value: string }[];
+  /**
+   * Specific url that will contain your image data after you've uploaded the file to the aws staged target.
+   */
+  resourceUrl?: string;
+  /**
+   * Url you'll use to post data to aws. It's a generic s3 url that when combined with the params sends your data to the right place.
+   */
+  url?: string;
+}
+
+export interface StagedUploadInput {
+  /**
+   * The size of the file to upload, in bytes.
+   * This is required when the request's resource property is set to `VIDEO` or `MODEL_3D`
+   */
+  fileSize?: number;
+  filename: string;
+  /**
+   * The HTTP method to be used when sending a request to upload the file using the returned staged upload target.
+   * Possible values are POST and PUT.
+   * @default POST
+   * */
+  httpMethod?: "POST" | "PUT";
+  /**
+   * The file's MIME type.
+   * */
+  mimeType: string;
+  resource: StagedUploadTargetGenerateUploadResource;
+}
+
+/*
+ *  ╔══════════════════════════════════════════════════════════════════════════════╗
  *  ║                               Pricing Types                                  ║
  *  ╚══════════════════════════════════════════════════════════════════════════════╝
  */
