96da5bfb33379bf4703a1e19dd6e38b514448e65
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-06-29 11:05:35 -0700

    ensures the children of panzoom can are visible

--- src/app/components/editor/PanZoom.tsx
+++ src/app/components/editor/PanZoom.tsx
@@ -1,58 +1,36 @@
 "use client";
 import useMediaQuery from "@/utils/hooks/useMediaQuery";
-import { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import {
   ReactZoomPanPinchContentRef,
   ReactZoomPanPinchProps,
+  ReactZoomPanPinchRef,
   TransformComponent,
   TransformWrapper,
-  useControls,
 } from "react-zoom-pan-pinch";
 
 interface PanZoomProps
   extends Omit<ReactZoomPanPinchProps, "pinch" | "initialScale"> {
-  children: React.ReactNode;
+  children: React.ReactElement;
   wrapperStyle?: React.CSSProperties;
   controlsRef?: React.MutableRefObject<ReactZoomPanPinchContentRef | null>;
 }
 
-interface PanZoomChildProps {
-  children: React.ReactNode;
-  wrapperStyle?: React.CSSProperties;
-  controlsRef?: React.MutableRefObject<ReactZoomPanPinchContentRef | null>;
-}
-
-const PanZoomChild = ({ controlsRef, ...props }: PanZoomChildProps) => {
-  const isMobile = useMediaQuery("(max-width: 768px)");
-  const controls = useControls();
-  useEffect(() => {
-    if (controlsRef) {
-      controlsRef.current = controls;
-    }
-  }, [controlsRef, controls]);
-  // zoom out on mobile
-  useEffect(() => {
-    if (isMobile) {
-      controls.resetTransform();
-    }
-  }, [isMobile, controls]);
-
-  return (
-    <TransformComponent
-      wrapperStyle={{
-        width: "100%",
-        height: "auto",
-        maxHeight: "100%",
-        overflow: isMobile ? "auto" : "hidden",
-        ...props.wrapperStyle,
-      }}
-    >
-      {props.children}
-    </TransformComponent>
-  );
+const getInitialScale = (
+  wrapper: HTMLElement,
+  child: HTMLElement,
+  defaultScale = 1
+) => {
+  const PADDING = 40;
+  const childBounds = child.getBoundingClientRect();
+  const wrapperBounds = wrapper.getBoundingClientRect();
+  const scaleX = (wrapperBounds.width - PADDING) / childBounds.width;
+  const scaleY = (wrapperBounds.height - PADDING) / childBounds.height;
+  // ensures no negative scale
+  return Math.min(scaleX, scaleY, defaultScale);
 };
 
-const PanZoom = ({
+const PanZoom: React.FC<PanZoomProps> = ({
   children,
   wheel,
   panning,
@@ -64,19 +42,44 @@ const PanZoom = ({
   disabled,
   controlsRef,
   ...props
-}: PanZoomProps) => {
+}) => {
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const childRef = useRef<HTMLElement | null>(null);
+
+  const handleInit = (ref: ReactZoomPanPinchRef) => {
+    if (controlsRef) {
+      controlsRef.current = ref;
+    }
+
+    if (childRef.current && ref.instance.wrapperComponent) {
+      const scale = getInitialScale(
+        ref.instance.wrapperComponent,
+        childRef.current
+      );
+      ref.zoomToElement(childRef.current, scale, 0);
+    }
+  };
+
+  useEffect(() => {
+    if (isMobile && controlsRef?.current) {
+      controlsRef.current.resetTransform();
+    }
+  }, [isMobile, controlsRef]);
+
+  // Clone the child element and pass the ref
+  const childWithRef =
+    children && React.cloneElement(children, { ref: childRef });
 
   return (
     <TransformWrapper
-      centerZoomedOut
       centerOnInit
       limitToBounds
       minScale={0.25}
       maxScale={3}
+      initialScale={1}
       disabled={isMobile || disabled}
       {...props}
-      initialScale={1}
+      onInit={handleInit}
       wheel={{
         step: 0.1,
         smoothStep: 0.002,
@@ -88,9 +91,7 @@ const PanZoom = ({
         activationKeys: ["Control", "Alt", "Meta", "Shift"],
         ...panning,
       }}
-      pinch={{
-        disabled: true,
-      }}
+      pinch={{ disabled: true }}
       doubleClick={{ disabled: true, ...doubleClick }}
       zoomAnimation={{
         disabled: false,
@@ -107,10 +108,19 @@ const PanZoom = ({
       }}
       velocityAnimation={{ disabled: true, ...velocityAnimation }}
     >
-      <PanZoomChild wrapperStyle={wrapperStyle} controlsRef={controlsRef}>
-        {children}
-      </PanZoomChild>
+      <TransformComponent
+        wrapperStyle={{
+          width: "100%",
+          height: "100%",
+          maxHeight: "100%",
+          overflow: isMobile ? "auto" : "hidden",
+          ...wrapperStyle,
+        }}
+      >
+        {childWithRef}
+      </TransformComponent>
     </TransformWrapper>
   );
 };
+
 export default PanZoom;
