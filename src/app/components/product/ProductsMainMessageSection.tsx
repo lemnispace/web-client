d50d56548f0c304b88e01bd9e4feba8093eab7b8
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-07-06 09:18:47 -0700

    add highlighting visuals to page header

--- src/app/components/product/ProductsMainMessageSection.tsx
+++ src/app/components/product/ProductsMainMessageSection.tsx
@@ -1,18 +1,50 @@
+import { Heading, HeadingProps } from "@/components/Heading";
 import clsx from "clsx";
 
-interface MainMessageSectionProps extends React.HTMLAttributes<HTMLDivElement> {
-  title: string;
+interface MainMessageSectionWithHighlightProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
   description: string;
+  headingProps?: HeadingProps
 }
 
+interface MainMessageSectionWithoutHighlightProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  description: string;
+  title: string;
+}
+
+type MainMessageSectionProps = MainMessageSectionWithHighlightProps | MainMessageSectionWithoutHighlightProps;
+
 export default function ProductsMainMessageSection({
-  title,
   description,
   className,
   ...props
 }: MainMessageSectionProps) {
+  if("headingProps" in props && props.headingProps){
+    const { headingProps, ...rest } = props;
+    return (
+      <div className={clsx("py-24 text-center", className)} {...rest}>
+      <Heading
+        {...headingProps}
+        className={clsx(
+          "text-4xl font-bold font-display tracking-tight",
+          headingProps.className
+        )}
+      />
+      <p className="mx-auto mt-4 max-w-3xl text-base text-gray-500">
+        {description}
+      </p>
+    </div>
+    );
+  }
+
+  if(!("title" in props)){
+    throw new Error("title is required for MainMessageSectionWithoutHighlightProps");
+  }
+  const {title, ...rest} = props;
+
   return (
-    <div className={clsx("py-24 text-center", className)} {...props}>
+    <div className={clsx("py-24 text-center", className)} {...rest}>
       <h1 className="text-4xl font-bold font-display tracking-tight text-gray-900">
         {title}
       </h1>
