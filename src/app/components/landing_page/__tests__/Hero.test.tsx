c4b328241a46f7e5dc1956a2b536551ebb7757e5
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-07-06 07:36:14 -0700

    Update Hero component to use Heading instead of Title

--- src/app/components/landing_page/__tests__/Hero.test.tsx
+++ src/app/components/landing_page/__tests__/Hero.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import { Hero, Title } from "../Hero";
+import { Heading, Hero } from "../Hero";
 
 // mock GLOBAL_APP_TEXT
 jest.mock("@/utils/text", () => ({
@@ -26,7 +26,7 @@ describe("Hero", () => {
   // test title doesn't render last line if not provided
   it("renders the title without last line", () => {
     const { getByTestId } = render(
-      <Title
+      <Heading
         firstLine="first"
         highlightedLine="second"
         lastLine={undefined}
@@ -34,23 +34,37 @@ describe("Hero", () => {
       />
     );
     const titleElement = getByTestId("test-title");
-    expect(titleElement).toBeInTheDocument();
     expect(titleElement).toHaveTextContent("first second");
   });
 
   // test title doesn't render first line if not provided
   it("renders the title without the firstl line", () => {
     const { getByTestId } = render(
-      <Title
+      <Heading
         highlightedLine="second"
         lastLine="last"
         data-testid="test-title"
       />
     );
     const titleElement = getByTestId("test-title");
-    expect(titleElement).toBeInTheDocument();
     expect(titleElement).toHaveTextContent("second last");
   });
+
+  it.each(["h1", "h2", "h3", "h4", "h5", "h6"])(
+    "renders the title using the %s tag",
+    (headingTag) => {
+      const { getByTestId } = render(
+        <Heading
+          highlightedLine="second"
+          lastLine="last"
+          data-testid="test-title"
+          as={headingTag as "h1" | "h2" | "h3" | "h4" | "h5" | "h6"}
+        />
+      );
+      const titleElement = getByTestId("test-title");
+      expect(titleElement.tagName).toBe(headingTag.toUpperCase());
+    }
+  );
 
   it("renders the description", () => {
     const { getByText } = render(<Hero />);
